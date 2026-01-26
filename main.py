aa84d8dda64dcf812fb29ffa97fddf7f289015a4
edinardofilho <edinardofreitas@gmail.com>
2026-01-26 02:15:43 -0300

    On branch main Your branch is up to date with 'origin/main'. Changes to be committed: 	modified:   main.py Changes not staged for commit: 	modified:   Classes/Flood_Fill.py 	deleted:    Classes/Textos.py 	modified:   Classes/__pycache__/Flood_Fill.cpython-312.pyc 	modified:   Classes/__pycache__/Textos.cpython-312.pyc 	modified:   main.py Untracked files: 	Classes/Tela_Abertura.py 	Classes/__pycache__/Tela_Abertura.cpython-312.pyc

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 import pygame
 import sys
-from Classes.Flood_Fill import Flood_Fill
-from Classes.Textos import Textos
+from Classes.Tela_Abertura import Tela_Abertura
 sys.setrecursionlimit(10**7)
 
 pygame.init()
 largura, altura = 800, 600
-cor_fundo = (100, 149, 237)  # Azul claro
-cor_preenchimento = (47, 79, 79)  #Cinza escuro
-cor_borda = (72, 209, 204)  # Turquesa
-poligono_caixa = [(100, 100), (400, 100), (400, 300), (100, 300)]
+azul_claro = (100, 149, 237)
 
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("ZLog Ltda")
 
-tela_abertura = Flood_Fill(tela)
-palavra_z = Textos(tela)
+tela_abertura = Tela_Abertura(tela)
 
 rodando = True
 
@@ -24,12 +19,9 @@ while rodando:
         if evento.type == pygame.QUIT:
             rodando = False
         
-    tela.fill(cor_fundo) # Preenche a tela com azul claro
+    tela.fill(azul_claro) # Preenche a tela com azul claro
 
-    tela_abertura.desenhar_poligono(poligono_caixa, cor_borda) # Desenha o retângulo da caixa
-    tela_abertura.flood_fill(250, 200, cor_preenchimento, cor_borda) # Preenche o retângulo com cinza escuro
-
-    palavra_z.desenhar_z(cor_preenchimento, cor_borda)  # Desenha o "Z" no centro da tela
+    
 
     pygame.display.flip()
 
